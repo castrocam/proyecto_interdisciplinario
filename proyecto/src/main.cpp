80607d7a800207c4fe6ed09510f23028e7e1d244
Cami Castro <camcastro@itba.edu.ar>
2025-09-25 20:33:34 -0300

    inspo codigo de display LCD I2C --> no lo probe

--- proyecto/src/main.cpp
+++ proyecto/src/main.cpp
@@ -1,18 +1,32 @@
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Def. de la dirección I2C y las dimensiones 
+// del LCD --> investigar si es 0x27 o 0x3f
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Inicializa el LCD
+    lcd.init();
+    
+    // Enciende la luz de fondo (backlight)
+    lcd.backlight();
+    
+    // Muestra un mensaje inicial
+    lcd.setCursor(0, 0); // Columna 0, Fila 0
+    lcd.print("Hola, ESP8266");
+    
+    lcd.setCursor(0, 1); // Columna 0, Fila 1
+    lcd.print("Con I2C y PIO!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    static int counter = 0;
+    
+    // Mueve el cursor a la segunda línea, columna 13
+    lcd.setCursor(13, 1); 
+    lcd.print(counter);
+    
+    delay(1000); // Espera 1 segundo
+    counter++;
 }
